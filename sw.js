e06b878ca27841c1eeee61515d1c5786074e5ab0
harshitap649-prog <harshitap649@gmail.com>
2025-11-02 00:30:41 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Service Worker - Currently not in use
+// Adsterra ads are loaded directly via script tags
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
